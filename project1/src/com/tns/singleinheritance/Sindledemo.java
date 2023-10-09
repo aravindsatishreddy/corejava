144bc4b769b263d6a1a582ee0ec69124f5b96d13
PC <PC@DESKTOP-PST83UM.bbrouter>
2023-10-09 09:38:10 +0530

    commit

---
+++ project1/src/com/tns/singleinheritance/Sindledemo.java
@@ -0,0 +1,11 @@
+package com.tns.singleinheritance;
+
+public class Sindledemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student st = new Student("aravind",98211813,"penugonda",833391442);
+		System.out.println(st);
+	}
+
+}

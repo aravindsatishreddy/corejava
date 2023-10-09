d4e7c03ec10ef1a9023d4ea71dacf4f547d51e75
PC <PC@DESKTOP-PST83UM.bbrouter>
2023-10-09 09:53:14 +0530

    commit

---
+++ project1/src/com/tns/abstraction/Square.java
@@ -0,0 +1,33 @@
+package com.tns.abstraction;
+
+public class Square extends Shape {
+		private float side;
+
+		
+		public Square() {
+			super();
+			side=2.0f;
+		}
+
+
+		public Square(float side) {
+			super();
+			this.side = side;
+		}
+
+
+		@Override
+		void calArea() {
+			// TODO Auto-generated method stub
+			super.area=side*side;
+		}
+
+
+		@Override
+		public String toString() {
+			return "Square [side=" + side + "]";
+		}
+		
+
+	}
+

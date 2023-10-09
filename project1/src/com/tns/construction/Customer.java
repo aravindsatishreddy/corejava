493cc668411b49790456a086b5ffe67a4a1ff5f2
PC <PC@DESKTOP-PST83UM.bbrouter>
2023-10-09 09:25:22 +0530

    commit

---
+++ project1/src/com/tns/construction/Customer.java
@@ -0,0 +1,43 @@
+package com.tns.construction;
+
+public class Customer {
+	private String customerName;
+	private int customerId;
+	private String customerCity;
+	//Default Constructor
+	public Customer() {
+		
+	}
+	
+	//Parameterized Constructor
+	public Customer(String customerName, int customerId, String customerCity) {
+		this.customerName = customerName;
+		this.customerId = customerId;
+		this.customerCity = customerCity;
+	}
+	//getters and setters
+	public String getCustomerName() {
+		return customerName;
+	}
+	
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getCustomerCity() {
+		return customerCity;
+	}
+	public void setCustomerCity(String customerCity) {
+		this.customerCity = customerCity;
+	}
+	@Override
+	public String toString() {
+		return "Customer [customerName=" + customerName + ", customerId=" + customerId + ", customerCity="
+				+ customerCity + "]";
+}}

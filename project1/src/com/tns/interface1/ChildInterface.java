1f0c80c3e59e0ae93d82a257e5bb311300227a5d
PC <PC@10.0.0.126>
2023-10-10 14:26:57 +0530

    commit1

---
+++ project1/src/com/tns/interface1/ChildInterface.java
@@ -0,0 +1,5 @@
+package com.tns.interface1;
+
+public interface ChildInterface {
+    void show();
+}
---
+++ project1/src/com/tns/interface1/InterfaceOne.java
@@ -0,0 +1,5 @@
+package com.tns.interface1;
+
+public interface InterfaceOne {
+	void print();
+}
